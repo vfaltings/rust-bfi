957b65158aea0260e2e5cd934728b430618b3d52
vfaltings <victor.faltings@gmail.com>
2022-06-22 18:50:02 +0200

    main loop functionality

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use std::env;
+use std::fs;
+use std::process;
+
+use rbfi::*;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 {
+        eprintln!("Insufficient arguments");
+        eprintln!("Usage: {} <FILE>", args[0]);
+    }
+
+    let filename = &args[1];
+
+    let contents: Vec<char> = fs::read_to_string(filename).unwrap_or_else(|err| {
+        eprintln!("Error reading file: {}", err);
+        process::exit(1);
+    }).chars().collect();
+
+    let mut env = Program::BFEnv::new();
+    let tokens = Lexer::tokenize(&contents);
+    let instructions = Parser::parse(&tokens);
+
+    env.run(&instructions);
 }
